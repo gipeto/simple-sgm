7b262d598ee373f697c02e32fedfbc6a9c278a69
Javier Perez <202426+gipeto@users.noreply.github.com>
2019-12-19 19:05:10 +0100

    Update test application and utilities

--- simple-sgm/apps/simple-sgm.cpp
+++ simple-sgm/apps/simple-sgm.cpp
@@ -1,40 +1,52 @@
 // simple-sgm.cpp : Defines the entry point for the console application.
 
+#include "utils.h"
+#include <iostream>
+#include <sgm/sgm.h>
 
-#include "stdafx.h"
-#define NOMINMAX
-#include <Windows.h>
-#include "Utils.h"
-#include "sgm.h"
+namespace
+{
+static constexpr int S_OK = 0;
+static constexpr int S_FAIL = -1;
 
-
+auto static constexpr DMin = 0;
 auto static constexpr DMax = 64;
 
+}  // namespace
 
-int main()
+int main(int argc, char* argv[])
 {
-	using namespace utils;
+    if (argc != 4)
+    {
+        std::cout << std::endl;
+        std::cout << "Computes a disparity map for the input left and right images" << std::endl;
+        std::cout << "The output image is saved as png" << std::endl;
+        std::cout << "Usage: simple-sgm <left-image-path> <right-image-path> <output-image-path>" << std::endl;
+        std::cout << std::endl;
+        return S_OK;
+    }
 
-	CoInitialize(NULL);
+    try
+    {
+        auto LeftImage = utils::io::readImage(argv[1]);
+        auto RightImage = utils::io::readImage(argv[2]);
 
-	auto Factory = CreateWICFactory();
+        if (LeftImage != RightImage)
+        {
+            throw std::runtime_error("Images must have the same dimension");
+        }
 
-	auto LeftImage  = ReadImage(Factory, L"im2.png");
-	auto RightImage = ReadImage(Factory, L"im6.png");
+        sgm::SemiGlobalMatching<DMax, DMin, true> Sgm(std::move(LeftImage), std::move(RightImage));
+        Sgm.SetPenalities(10, 80);
+        const auto DMap = Sgm.GetDisparity();
 
+        utils::io::saveImage(argv[3], DMap);
 
-	ASSERT(LeftImage == RightImage);
-
-	SimpleImage DMap;
-
-	{
-		PerformanceTimer Timer(L"Sgm");
-		 sgm::SemiGlobalMatching<DMax> Sgm(std::move(LeftImage), std::move(RightImage));
-		 Sgm.SetPenalities(10, 80);
-		 DMap = Sgm.GetDisparity();
-	}
-
-	    SaveImage(DMap,Factory, L"DisparityMap.png");
-		return 0;
+        return S_OK;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return S_FAIL;
+    }
 }
-
